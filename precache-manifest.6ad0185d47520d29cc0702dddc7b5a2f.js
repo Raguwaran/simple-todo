89d6f9de19d0df4fe6e72e1e845ebcaca947360f
raguwaran <raguwarans36@gmail.com>
2021-11-30 21:12:08 +0530

    Updates

---
+++ precache-manifest.6ad0185d47520d29cc0702dddc7b5a2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d59c2686e18a7ecdf5ae286236c2987c",
+    "url": "/simple-todo/index.html"
+  },
+  {
+    "revision": "54eae5af9bb7fce03b78",
+    "url": "/simple-todo/static/css/main.f4a9fc0b.chunk.css"
+  },
+  {
+    "revision": "3a6b957d3697d34b8887",
+    "url": "/simple-todo/static/js/2.398ce10e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo/static/js/2.398ce10e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54eae5af9bb7fce03b78",
+    "url": "/simple-todo/static/js/main.de3df51a.chunk.js"
+  },
+  {
+    "revision": "1cba16a75a1141474762",
+    "url": "/simple-todo/static/js/runtime-main.3d365e17.js"
+  }
+]);
